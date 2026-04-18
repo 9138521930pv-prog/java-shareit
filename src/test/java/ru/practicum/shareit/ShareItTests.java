77e3d88097cea75312cce4f0894af2c52eb64773
9138521930pv-prog <9138521930.pv@gmail.com>
2026-04-18 22:53:06 +0700

    Fix test error

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,13 +1,17 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class ShareItTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Тест проверяет, что контекст загружается
+    }
 }
